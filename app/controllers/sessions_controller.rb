39b5e6c6f87ba07c32de8cbc81213d5ac12c2fb9
maimuna omar <maymunaabdi07@gmail.com>
2023-09-12 22:17:28 +0300

    worked on session controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,44 @@
+# app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
 
+  def create
+    user = User.find_by(email: params[:email])
+  
+    if user && user.authenticate(params[:password])
+        token = SecureRandom.hex(20)
+        user.update(token: token)
+        render json: { status: 'logged in', user: user, token: token }
+    else
+        render json: { status: 'error', message: 'Incorrect email or password' }, status: :unauthorized
+    end
+  end
+  
+      # def create
+      #     user = User.find_by(email: params[:email])
+        
+      #     if user
+      #       if user.authenticate(params[:password])
+      #         render json: { status: 'logged in', user: user }
+      #       else
+      #         render json: { status: 'error', message: 'Incorrect password' }, status: :unauthorized
+      #       end
+      #     else
+      #       render json: { status: 'error', message: 'Email not found' }, status: :unauthorized
+      #     end
+      #   end
+        
+      # For logging out (optional for now)
+      # def destroy
+      #   # Logic to log out user
+      #   render json: { status: 'logged out' }
+      # end
+  
+      # app/controllers/sessions_controller.rb
+  
+  def destroy
+    @current_user.update(token: nil) if @current_user
+    render json: { status: 'logged out' }
+  end
+      
+    end
     
