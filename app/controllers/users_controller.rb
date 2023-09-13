40d1531038c56fdbd4fc699f56aa7862ba013776
maimuna omar <maymunaabdi07@gmail.com>
2023-09-13 13:03:12 +0300

    worked on users controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,15 +17,7 @@ class UsersController < ApplicationController
           render json: { errors: user.errors }, status: :unprocessable_entity
         end
       end
-      
-    # def create
-    #   user = User.new(user_params)
-    #   if user.save
-    #     render json: { user: user, status: :created }
-    #   else
-    #     render json: { errors: user.errors }, status: :unprocessable_entity
-    # end
-    # end
+   
   
     def show
       user = User.find(params[:id])
